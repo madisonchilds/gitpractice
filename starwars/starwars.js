656a0968b56d7e6a424af710b9a8c29f1f536c47
Madison Childs <78438105+madisonchilds@users.noreply.github.com>
2021-03-04 09:04:51 -0700

    adding starwars

---
+++ starwars/starwars.js
@@ -0,0 +1,26 @@
+import { films } from'../data/films.js'
+
+/*test*/
+let filmList = document.querySelector('#filmList')
+
+
+for (let i = 0; < films.length; i++) {
+
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
+    let posterFig = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    let figCaption = document.createElement('figcaption')
+
+    figCaption.textContent = foundFilm.title
+    posterFig.appendChild(figImg)
+    posterFig.appendChild(figCaption)
+
+    filmList.appendChild(posterFig)
+}
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    return url.charAt(end - 1)
+    
+}
