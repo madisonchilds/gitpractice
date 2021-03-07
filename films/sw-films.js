a33b927d1735467f21dc6fa9ba8b206575e92b1e
Madison Childs <78438105+madisonchilds@users.noreply.github.com>
2021-03-06 21:32:59 -0700

    needs to be fixed

--- films/sw-films.js
+++ films/sw-films.js
@@ -8,7 +8,7 @@ for (let i = 0; < films.length; i++) {
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
     let posterFig = document.createElement('figure')
     let figImg = document.createElement('img')
-    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/7.jpg`
     let figCaption = document.createElement('figcaption')
 
     figCaption.textContent = foundFilm.title
