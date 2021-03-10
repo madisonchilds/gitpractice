dcb80f97f5c67fc78bb6bb60247e91e9b5a58163
Madison Childs <78438105+madisonchilds@users.noreply.github.com>
2021-03-09 22:11:51 -0700

    adding characters

--- films/sw-films.js
+++ films/sw-films.js
@@ -1,14 +1,15 @@
 import { films } from'../data/films.js'
+import { people } from'../data/people.js'
 
 let filmList = document.querySelector('#filmList')
 
 
 for (let i = 0; i < films.length; i++) {
-
+    
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
     let posterFig = document.createElement('figure')
     let figImg = document.createElement('img')
-    figImg.src = `https://starwars-visualguide.com/assets/img/films/7.jpg`
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/` + (i + 1) + `.jpg`
     let figCaption = document.createElement('figcaption')
 
     figCaption.textContent = foundFilm.title
@@ -18,8 +19,28 @@ for (let i = 0; i < films.length; i++) {
     filmList.appendChild(posterFig)
 }
 
+for (let i = 0; i <= people.length; i++) {
+        const foundCharacter = people.find(character => getLastNumber(character.url) === (i + 1).toString())
+        
+        try{
+            let posterFig = document.createElement('figure')
+            let figImg = document.createElement('img')
+            figImg.src = `https://starwars-visualguide.com/assets/img/characters/` + (i + 1) + `.jpg`
+            let figCaption = document.createElement('figcaption')
+        
+            figCaption.textContent = foundCharacter.name
+            posterFig.appendChild(figImg)
+            posterFig.appendChild(figCaption)
+        
+            filmList.appendChild(posterFig)
+        } catch {
+            ;
+        }
+
+}
+
 function getLastNumber(url) {
     let end = url.lastIndexOf('/')
-    return url.charAt(end - 1)
-    
+    let beginning = url.lastIndexOf('/', url.lastIndexOf('/')-1)
+    return url.substring(beginning + 1, end)
 }
