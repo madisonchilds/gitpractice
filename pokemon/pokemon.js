00944e08c9d4ba8a02be950bc0bf6da3a0bf795f
Madison Childs <78438105+madisonchilds@users.noreply.github.com>
2021-04-19 11:28:16 -0600

    got api data

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -18,8 +18,14 @@ async function getAPIData(url) {
 function loadPage() {
     const response = getAPIData('https://pokeapi.co/api/v2/pokemon?limit=25').then(
         (data) => {
-            console.log(data)
+            for (const singlePokemon of data.results)
+            console.log(singlePokemon)
+            populatePokeCard(singlePokemon)
         }
     )
-    console.log(response)
+    
+}
+
+function populatePokeCard(singlePokemon) {
+    console.log(singlePokemon)
 }
