40feb0c75de44bc933c37249ae13299c2e886bd9
Madison Childs <78438105+madisonchilds@users.noreply.github.com>
2021-04-19 19:28:52 -0600

    resize images get everything situated

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -18,14 +18,58 @@ async function getAPIData(url) {
 function loadPage() {
     const response = getAPIData('https://pokeapi.co/api/v2/pokemon?limit=25').then(
         (data) => {
-            for (const singlePokemon of data.results)
-            console.log(singlePokemon)
-            populatePokeCard(singlePokemon)
+            for (const singlePokemon of data.results) {
+                await getAPIData(singlePokemon.url).then(
+                    (pokeData) => populatePokeCard(pokeData)
+                )
+           
+            // populatePokeCard(singlePokemon)
+            }
         }
     )
     
 }
 
 function populatePokeCard(singlePokemon) {
-    console.log(singlePokemon)
+    // console.log(singlePokemon)
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+
+    pokeCard.appendChild(populateCardFront(singlePokemon))
+    pokeCard.appendChild(populateCardBack(singlePokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
 }
+
+function populateCardFront(pokemon) {
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card_face card_face--front'
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = pokemon.name
+    let frontImage = document.createElement('img')
+    frontImage.src - `images/${getImageFileName(pokemon)}.png`
+
+    pokeFront.appendChild(frontLabel)
+    pokeFront.appendChild(frontImage)
+    return pokeFront
+}
+
+function populateCardBack(pokemon) {
+    let pokeBack = document.createElement('div')
+    pokeBack.className = 'card_face card_face--back'
+    let backLabel = document.createElement('p')
+    backLabel.textContent = `Moves: ${pokemon.moves.length}`
+    pokeBack.appendChild(backLabel)
+    return pokeBack
+}
+
+function getImageFileName(pokemon) {
+    if (pokemon.id < 10) {
+        return `00${pokemon.id}`
+    } else if (pokemon.id > 9 && pokemon.id < 100) {
+        return `0${pokemon.id}`
+    }
+
+    }
