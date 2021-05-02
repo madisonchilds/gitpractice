14fcf7c6962cad15cbb11c22fc72af430c17469f
Madison Childs <78438105+madisonchilds@users.noreply.github.com>
2021-05-02 13:43:13 -0600

    congress working and started styling

--- congress/congress.js
+++ congress/congress.js
@@ -1,9 +1,11 @@
 import { senators } from '../data/senators.js'
 import { representatives } from '../data/representatives.js'
 
-const congressGrid = document.querySelector('congressGrid')
+const congressGrid = document.querySelector('.congressGrid')
 const seniorityButton = document.querySelector('#seniorityButton')
 const birthdayButton = document.querySelector('#birthdayButton')
+const missedVotesButton = document.querySelector('#missedVotes')
+const partyHackButton = document.querySelector('#partyHack')
 
 seniorityButton.addEventListener('click', () => {
     senioritySort()
@@ -11,6 +13,14 @@ seniorityButton.addEventListener('click', () => {
 
 birthdayButton.addEventListener('click', () => {
     birthdaySort()
+})
+
+missedVotesButton.addEventListener('click', () => {
+    alert(`${missedVotesRep.name} missed votes ${missedVotesRep.missed_votes_pct}% of the time!`)
+})
+
+partyHackButton.addEventListener('click', () => {
+    alert(`There are ${partyHackArray.length} representatives who vote with their party ${partyHack.votes_with_party_pct}% of the time!`)
 })
 
 function populateCongressGrid(simplePeople) {
@@ -40,18 +50,48 @@ function getSimplifiedCongress(congressPeople) {
             title: person.title,
             name: `${person.first_name} ${middleName} ${person.last_name}`,
             imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`,
+            seniority: parseInt(person.seniority, 10),
+            date_of_birth: parseInt(person.date_of_birth, 10),
+            missed_votes_pct: person.missed_votes_pct,
+            votes_with_party_pct: person.votes_with_party_pct
         }
     })
 }
 
 function senioritySort() {
-    populateCongressGrid(getSimplifiedCongress(senators).sort(
-       (a, b) => a.seniority - b.seniority
-   ).reverse())
+     populateCongressGrid(getSimplifiedCongress(senators).sort(
+        (a, b) => a.seniority - b.seniority
+    ).reverse())
 }
 
 function birthdaySort() {
-   populateCongressGrid(getSimplifiedCongress(senators).sort(
-       (a, b) => a.date_of_birth - b.date_of_birth
-   ))
+    populateCongressGrid(getSimplifiedCongress(senators).sort(
+        (a, b) => a.date_of_birth - b.date_of_birth
+    ))
+}
+
+const missedVotesRep = getSimplifiedCongress(representatives).filter((rep) => rep.title === 'Representative').reduce((acc, rep) => acc.missed_votes_pct > rep.missed_votes_pct ? acc : rep)
+
+const partyHack = getSimplifiedCongress(representatives).filter((rep) => rep.title === 'Representative').reduce((acc, rep) => acc.votes_with_party_pct > rep.votes_with_party_pct ? acc : rep)
+
+const partyHackArray = getSimplifiedCongress(representatives).filter((person) => {
+    return person.votes_with_party_pct === partyHack.votes_with_party_pct
+})
+
+populateCongressGrid(getSimplifiedCongress(senators))
+
+  
+export function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+export function removeChildren(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
 }
